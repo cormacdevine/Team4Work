e8e605ac4ed7050151060c1fc9129a90f4e9d4f4
cormacdevine <devine-c44@ulster.ac.uk>
2024-12-02 13:18:03 +0000

    Add project files.

---
+++ groupWork/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupWork
+{
+    using System;
+
+    public class Order
+    {
+        // Private variables
+        private int orderId;
+        private string customerName;
+        private decimal orderAmount;
+
+        // Constructor
+        public Order(int orderId, string customerName, decimal orderAmount)
+        {
+            this.orderId = orderId;
+            this.customerName = customerName;
+            this.orderAmount = orderAmount;
+        }
+
+        // Public properties (using get and set)
+        public int OrderId
+        {
+            get { return orderId; }
+            set { orderId = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+        public decimal OrderAmount
+        {
+            get { return orderAmount; }
+            set { orderAmount = value; }
+        }
+
+        // Other methods (you can add more as needed)
+        public void ProcessOrder()
+        {
+            // Implement order processing logic here
+            Console.WriteLine($"Processing order {orderId} for customer {customerName}...");
+            // Additional logic goes here
+        }
+    }
+    
+}
---
+++ groupWork/Class2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupWork
+{
+
+    public class Shipment
+    {
+        // Private variables
+        private int shipmentId;
+        private string destination;
+        private DateTime shipmentDate;
+
+        // Constructor
+        public Shipment(int shipmentId, string destination, DateTime shipmentDate)
+        {
+            this.shipmentId = shipmentId;
+            this.destination = destination;
+            this.shipmentDate = shipmentDate;
+        }
+
+        // Public properties 
+        public int ShipmentId
+        {
+            get { return shipmentId; }
+            set { shipmentId = value; }
+        }
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        public DateTime ShipmentDate
+        {
+            get { return shipmentDate; }
+            set { shipmentDate = value; }
+        }
+
+        // Other methods (you can add more as needed)
+        public void Ship()
+        {
+            // Implement shipment logic here
+            Console.WriteLine($"Shipping order {shipmentId} to {destination} on {shipmentDate.ToShortDateString()}...");
+            // Additional logic goes here
+        }
+    }
+}
